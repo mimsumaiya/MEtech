f902e3ec86669001caebed4a82baa160cf7f2db6
Sumaiya Mim <ssumaiyamim2000@gmail.com>
2020-04-19 08:40:37 +0600

    new update

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+<div style={{ color:'purple' , fontSize: '52px'}}>
+    <Header/>
+    Hello world! about
+</div>)
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default () => (<div style={{ color:'purple' , fontSize: '52px'}}>Hello world! Mimcxcxc
+<button className="primary-button">button</button>
+
+</div>)
